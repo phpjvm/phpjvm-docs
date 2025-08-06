141113d3f6f6a52f09a2c2af2ae71518ac8c8a73
Meigo™ Corporation <73817505+meigoc@users.noreply.github.com>
2025-08-06 21:59:42 +0300

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+const IconHighPerformance = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="#25c2a0" viewBox="0 0 16 16" {...props}>
+    <path d="M7.596 7.304a.802.802 0 0 1 0 1.392l-6.363 3.692C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696z"/>
+    <path d="M15.596 7.304a.802.802 0 0 1 0 1.392l-6.363 3.692C8.713 12.69 8 12.345 8 11.692V4.308c0-.653.713-.998 1.233-.696z"/>
+  </svg>
+);
+
+const IconNewApi = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="#712cf9" viewBox="0 0 16 16" {...props}>
+    <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1"/>
+  </svg>
+);
+
+const IconGuiGameAndroid = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="#3DDC84" viewBox="0 0 16 16" {...props}>
+    <path d="m10.213 1.471.691-1.26q.069-.124-.048-.192-.128-.057-.195.058l-.7 1.27A4.8 4.8 0 0 0 8.005.941q-1.032 0-1.956.404l-.7-1.27Q5.281-.037 5.154.02q-.117.069-.049.193l.691 1.259a4.25 4.25 0 0 0-1.673 1.476A3.7 3.7 0 0 0 3.5 5.02h9q0-1.125-.623-2.072a4.27 4.27 0 0 0-1.664-1.476ZM6.22 3.303a.37.37 0 0 1-.267.11.35.35 0 0 1-.263-.11.37.37 0 0 1-.107-.264.37.37 0 0 1 .107-.265.35.35 0 0 1 .263-.11q.155 0 .267.11a.36.36 0 0 1 .112.265.36.36 0 0 1-.112.264m4.101 0a.35.35 0 0 1-.262.11.37.37 0 0 1-.268-.11.36.36 0 0 1-.112-.264q0-.154.112-.265a.37.37 0 0 1 .268-.11q.155 0 .262.11a.37.37 0 0 1 .107.265q0 .153-.107.264M3.5 11.77q0 .441.311.75.311.306.76.307h.758l.01 2.182q0 .414.292.703a.96.96 0 0 0 .7.288.97.97 0 0 0 .71-.288.95.95 0 0 0 .292-.703v-2.182h1.343v2.182q0 .414.292.703a.97.97 0 0 0 .71.288.97.97 0 0 0 .71-.288.95.95 0 0 0 .292-.703v-2.182h.76q.436 0 .749-.308.31-.307.311-.75V5.365h-9zm10.495-6.587a.98.98 0 0 0-.702.278.9.9 0 0 0-.293.685v4.063q0 .406.293.69a.97.97 0 0 0 .702.284q.42 0 .712-.284a.92.92 0 0 0 .293-.69V6.146a.9.9 0 0 0-.293-.685 1 1 0 0 0-.712-.278m-12.702.283a1 1 0 0 1 .712-.283q.41 0 .702.283a.9.9 0 0 1 .293.68v4.063a.93.93 0 0 1-.288.69.97.97 0 0 1-.707.284 1 1 0 0 1-.712-.284.92.92 0 0 1-.293-.69V6.146q0-.396.293-.68"/>
+  </svg>
+);
+
 const FeatureList = [
   {
     title: 'High Performance',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: IconHighPerformance,
     description: (
       <>
         JIT + Optimizer = 1x - 10x faster PHP 5.6
@@ -14,7 +33,7 @@ const FeatureList = [
   },
   {
     title: 'New API',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: IconNewApi,
     description: (
       <>
         A better std library, more elegant and powerful
@@ -23,7 +42,7 @@ const FeatureList = [
   },
   {
     title: 'GUI, Game, Android',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: IconGuiGameAndroid,
     description: (
       <>
         Use the php language not only for web apps
